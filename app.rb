55f9f44ccbd8a1ed043f634e4f4145bf48bc5c13
Stephen <sid@latterfamily.com>
2014-04-27 20:54:35 +0100

    Initial commit.
    Adventures in Ruby, Sinatra, and CouchDB.

---
+++ app.rb
@@ -0,0 +1,56 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'rest_client'
+require 'json'
+require 'ostruct'
+
+DB_URL = 'http://localhost:5984/testy'
+
+get '/' do
+  "Hello World!"
+end
+
+post "/upload" do 
+  File.open('public/uploads/images/' + params['file'][:filename], "w") do |f|
+    f.write(params['file'][:tempfile].read)
+  end
+  "/uploads/images/#{params['file'][:filename]}"
+end
+
+post "/save" do
+  page = JSON.parse(params['page'])
+  content_type :json
+  RestClient.put("#{DB_URL}/#{page['_id']}", params['page'], {:content_type => :json})
+end
+
+get '/*' do
+  page = get_page(params[:splat].join('/').to_s)
+  content = get_content(page)
+  # TODO: make the template to load dynamic from the page object
+  erb :page, locals: {page: page, content: content} 
+end
+
+
+
+
+def get_page(id)
+  begin
+    OpenStruct.new(JSON.parse(RestClient.get("#{DB_URL}/#{id}")))
+  rescue
+    OpenStruct.new({title: "Page not found" })
+  end
+end
+
+def get_content(page)
+  content = {}
+  page.content.each do |zone, components|
+    components.each_with_index do |component, i|
+      component = OpenStruct.new(component)
+      views  = "components/#{component.name}"
+      layout_options = { :views => 'views' }
+      locals = {component: component, index: i}
+      (content[zone] ||= '') << erb(:view, {views: views, layout: :component, layout_options: layout_options, locals: locals})
+    end
+  end
+  OpenStruct.new(content)
+end
