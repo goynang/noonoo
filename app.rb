fa316a0075b54743aa6184ca85b889bac8044cef
Stephen <goynang@users.noreply.github.com>
2015-07-25 10:14:40 +0100

    Seed some data if first run.

--- app.rb
+++ app.rb
@@ -11,7 +11,68 @@ module NooNoo
     set :app_file, __FILE__
     
     helpers NooNoo::Helpers
-
+    
+    # If we have no pages then intialise the system (so is usable!)
+    # Rather messy but will do for now...
+    if NooNoo::Models::Page.empty?
+      NooNoo::Models::Page.create({
+        path: '/',
+        title: 'Welcome to this here thing',
+        zones: {
+          primary: [
+            {
+              name: "heading",
+              content: "Get the admin panel open"
+            }, {
+              name: "text",
+              content: "<p>Click the little thing hanging off the right hand edge of the page to get going.</p>"
+            }, {
+              name: "heading",
+              content: "Terms used"
+            }, {
+              name: "subheading",
+              content: "Page"
+            }, {
+              name: "text",
+              content: "<p>A <em>page</em> is... um, a page. Simple. Pages have a <em>layout</em> that determines the <em>zones</em> in the page where you can place content <em>elements</em>.</p>"
+            }, {
+              name: "subheading",
+              content: "Layout"
+            }, {
+              name: "text",
+              content: "<p>A <em>layout</em> determines the <em>zones</em> a <em>page</em> has.</p>"
+            }, {
+              name: "subheading",
+              content: "Zone"
+            }, {
+              name: "text",
+              content: "<p>A <em>zone</em> is an area where you can place content <em>elements</em>. Zones are either tied to a <em>layout</em> (for sharing content across pages) or to indiviudal <em>pages</em>.</p>"
+            }, {
+              name: "subheading",
+              content: "Element (aka component)"
+            }, {
+              name: "text",
+              content: "<p>A content <em>element</em> is the basic building block that makes a <em>page</em> interesting. You scatter them around to build your <em>page</em> up.</p>"
+            }
+          ],
+          secondary: []
+        },
+        columns: 1,
+        depth: 1,
+        label: 'Home',
+        show_in_menus: true,
+        show_on_sitemap: true,
+        layout: NooNoo::Models::Layout.create({
+          title: 'Default',
+          zones: {
+            nav:[{name: 'menu'}],
+            heading:[{name: 'page-title'}],
+            foot:[]
+          }
+        })
+      })
+    end
+    
   end
 end
 
